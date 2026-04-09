e4454bc393c8e12bea3922d31146b7b88194e377
Huong Nguyen <vnnvanhuong@gmail.com>
2026-04-09 20:41:30 +0700

    Add: merge-two-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-1.go
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+
+// Recursion approach
+// base case: at least one of the list is empty
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+
+	if list2 == nil {
+		return list1
+	}
+	
+	if list1.Val <= list2.Val {
+		list1.Next = mergeTwoLists(list1.Next, list2)
+		return list1
+	}
+
+	list2.Next = mergeTwoLists(list1, list2.Next)
+	return list2
+    
+}
