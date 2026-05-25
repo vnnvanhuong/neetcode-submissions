26f0311c32d9f0ec34a18691936be5299618976f
Huong Nguyen <vnnvanhuong@gmail.com>
2026-05-25 23:03:29 +0700

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.go
@@ -0,0 +1,20 @@
+
+func search(nums []int, target int) int {
+	left, right := 0, len(nums) -1
+
+	for left <= right {
+		mid := (left+right)/2
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[mid] > target {
+			right = mid-1
+			continue
+		}
+
+		left = mid + 1
+	}
+
+	return -1
+}
